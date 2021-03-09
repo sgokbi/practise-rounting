a1ec866e69c80646bb35fcba241e8423c0a95d2a
SagorikaKubi <sagorika.tech@gmail.com>
2021-03-09 15:35:06 +0600

    added

--- src/components/Friend/Friend.js
+++ src/components/Friend/Friend.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 const Friend = (props) => {
     const { id, name, email, phone } = props.friend;
